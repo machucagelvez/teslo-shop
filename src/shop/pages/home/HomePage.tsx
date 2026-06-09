a1a00d3f42031709b2d2164b1754414ebb33f414
Fabián <machucagelvez@gmail.com>
2026-06-09 18:18:01 -0500

    products and pagination

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,16 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/product.mock";
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductGrid } from "@/shop/components/ProductGrid";
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumbotron title="Todos los productos" />
+
+      <ProductGrid products={products} />
+
+      <CustomPagination totalPages={5} />
     </>
   );
 };
