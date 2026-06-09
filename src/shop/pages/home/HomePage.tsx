862cbc57ef4bdadc2a84e4fd38ff9297d6b3871c
Fabián <machucagelvez@gmail.com>
2026-06-09 16:18:34 -0500

    routes + design

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <h1 className="font-montserrat">Hola Mundo</h1>
+      HomePage
+      <Button>Button</Button>
+    </div>
+  );
 };
