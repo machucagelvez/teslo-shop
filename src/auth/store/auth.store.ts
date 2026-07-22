3a3a17382af508724a9cb7ee678867497c68c11c
Fabián <machucagelvez@gmail.com>
2026-07-22 11:45:31 -0500

    verify auth status

--- src/auth/store/auth.store.ts
+++ src/auth/store/auth.store.ts
@@ -1,6 +1,7 @@
 import type { User } from "@/interfaces/user.interface";
 import { create } from "zustand";
 import { loginAction } from "../actions/login.action";
+import { checkAuthAction } from "../actions/check-auth.action";
 
 type AuthStatus = "authenticated" | "not-authenticated" | "checking";
 
@@ -15,6 +16,7 @@ type AuthState = {
   //Actions
   login: (email: string, password: string) => Promise<boolean>;
   logout: () => void;
+  checkAuthStatus: () => Promise<boolean>;
 };
 
 export const useAuthStore = create<AuthState>()((set) => ({
@@ -29,17 +31,36 @@ export const useAuthStore = create<AuthState>()((set) => ({
     try {
       const data = await loginAction(email, password);
       localStorage.setItem("token", data.token);
-      set({ user: data.user, token: data.token });
+      set({ user: data.user, token: data.token, authStatus: "authenticated" });
       return true;
     } catch (error) {
       localStorage.removeItem("token");
-      set({ user: null, token: null });
+      set({ user: null, token: null, authStatus: "not-authenticated" });
       return false;
     }
   },
 
   logout: () => {
     localStorage.removeItem("token");
-    set({ user: null, token: null });
+    set({ user: null, token: null, authStatus: "not-authenticated" });
+  },
+
+  checkAuthStatus: async () => {
+    try {
+      const { token, user } = await checkAuthAction();
+      set({
+        user,
+        token,
+        authStatus: "authenticated",
+      });
+      return true;
+    } catch (error) {
+      set({
+        user: undefined,
+        token: undefined,
+        authStatus: "authenticated",
+      });
+      return false;
+    }
   },
 }));
